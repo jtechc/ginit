c8118aa083ca1401e95906db9750cfb147304698
Zack <zytesas@Zack-Main.localdomain>
2021-05-21 15:44:13 -0400

    2021-05-21 15:44:13

--- app.js
+++ app.js
@@ -5,15 +5,6 @@ import { directoryExists } from './lib/files.js';
 import { getStoredGitHubToken, getPersonalAccessToken, gitHubAuth } from './lib/github.js';
 import { createRemoteRepo, createGitIgnore, setupRepo } from './lib/repo.js';
 
-/*const chalk = require('chalk');
-const clear = require('clear');
-const figlet = require('figlet');
-const touch = require('touch');
-const files = require('./lib/files');
-const github = require('./lib/github');
-const repo = require('./lib/repo');
-const inquirer = require('./lib/inquirer');
-*/
 clear();
 
 console.log(
